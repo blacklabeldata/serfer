da3d4b5120d2f0f00ea671c25b45f721de71e14e
eliquious <eliquious@gmail.com>
2015-09-27 20:33:11 -0500

    Add more granular user event processing and configurable reconiliation
    - Add leader election handler
    - Add unknown event handler
    - Add reconciliation options
    - Add service prefix to event handling
    - Add IsLeader function

--- handler.go
+++ handler.go
@@ -1,8 +1,16 @@
 package serfer
 
 import (
+	"strings"
+
 	"github.com/hashicorp/serf/serf"
 	log "github.com/mgutz/logxi/v1"
+)
+
+const (
+	// StatusReap is used to update the status of a node if we
+	// are handling a EventMemberReap
+	StatusReap = serf.MemberStatus(-1)
 )
 
 // EventHandler processes generic Serf events. Depending on the
@@ -28,11 +36,47 @@ type QueryEventHandler interface {
 
 // Reconciler is used to reconcile Serf events wilth an external process, like Raft.
 type Reconciler interface {
-	Reconcile(serf.MemberEvent)
+	Reconcile(serf.Member)
 }
+
+// IsLeaderFunc should return true if the local node is the cluster leader.
+type IsLeaderFunc func() bool
 
 // SerfEventHandler is used to dispatch various Serf events to separate event handlers.
 type SerfEventHandler struct {
+
+	// ServicePrefix is used to filter out unknown events.
+	ServicePrefix string
+
+	// ReconcileOnJoin determines if the Reconiler is called when a node joins the cluster.
+	ReconcileOnJoin bool
+
+	// ReconcileOnLeave determines if the Reconiler is called when a node leaves the cluster.
+	ReconcileOnLeave bool
+
+	// ReconcileOnFail determines if the Reconiler is called when a node fails.
+	ReconcileOnFail bool
+
+	// ReconcileOnUpdate determines if the Reconiler is called when a node updates.
+	ReconcileOnUpdate bool
+
+	// ReconcileOnReap determines if the Reconiler is called when a node is reaped from the cluster.
+	ReconcileOnReap bool
+
+	// IsLeader determines if the local node is the cluster leader.
+	IsLeader IsLeaderFunc
+
+	// IsLeaderEventFunc determines if an event is a leader election event based on the event name.
+	IsLeaderEvent func(string) bool
+
+	// LeaderElectionHandler processes leader election events.
+	LeaderElectionHandler UserEventHandler
+
+	// UserEvent processes known, non-leader election events.
+	UserEvent UserEventHandler
+
+	// UnknownEventHandler processes unkown events.
+	UnknownEventHandler UserEventHandler
 
 	// Called when a Member joins the cluster.
 	NodeJoined MemberEventHandler
@@ -55,9 +99,6 @@ type SerfEventHandler struct {
 	// Called when a serf.Query is received.
 	QueryHandler QueryEventHandler
 
-	// Called when a user event is received.
-	UserEvent UserEventHandler
-
 	// Logs output
 	Logger log.Logger
 }
@@ -75,7 +116,7 @@ func (s SerfEventHandler) HandleEvent(e serf.Event) {
 	// If the event is a Join event, call NodeJoined and then reconcile event with
 	// persistent storage.
 	case serf.EventMemberJoin:
-		reconcile = true
+		reconcile = s.ReconcileOnJoin
 		if s.NodeJoined != nil {
 			s.NodeJoined.HandleMemberEvent(e.(serf.MemberEvent))
 		}
@@ -83,7 +124,7 @@ func (s SerfEventHandler) HandleEvent(e serf.Event) {
 	// If the event is a Leave event, call NodeLeft and then reconcile event with
 	// persistent storage.
 	case serf.EventMemberLeave:
-		reconcile = true
+		reconcile = s.ReconcileOnLeave
 		if s.NodeLeft != nil {
 			s.NodeLeft.HandleMemberEvent(e.(serf.MemberEvent))
 		}
@@ -91,27 +132,25 @@ func (s SerfEventHandler) HandleEvent(e serf.Event) {
 	// If the event is a Failed event, call NodeFailed and then reconcile event with
 	// persistent storage.
 	case serf.EventMemberFailed:
-		reconcile = true
+		reconcile = s.ReconcileOnFail
 		if s.NodeFailed != nil {
 			s.NodeFailed.HandleMemberEvent(e.(serf.MemberEvent))
 		}
 
 	// If the event is a Reap event, reconcile event with persistent storage.
 	case serf.EventMemberReap:
-		reconcile = true
+		reconcile = s.ReconcileOnReap
 		if s.NodeReaped != nil {
 			s.NodeReaped.HandleMemberEvent(e.(serf.MemberEvent))
 		}
 
-	// If the event is a user event, call UserEvent
+	// If the event is a user event, handle leader elections, user events and unknown events.
 	case serf.EventUser:
-		if s.UserEvent != nil {
-			s.UserEvent.HandleUserEvent(e.(serf.UserEvent))
-		}
+		s.handleUserEvent(e.(serf.UserEvent))
 
 	// If the event is an Update event, call NodeUpdated
 	case serf.EventMemberUpdate:
-		reconcile = true
+		reconcile = s.ReconcileOnUpdate
 		if s.NodeUpdated != nil {
 			s.NodeUpdated.HandleMemberEvent(e.(serf.MemberEvent))
 		}
@@ -128,6 +167,76 @@ func (s SerfEventHandler) HandleEvent(e serf.Event) {
 
 	// Reconcile event with external storage
 	if reconcile && s.Reconciler != nil {
-		s.Reconciler.Reconcile(e.(serf.MemberEvent))
+		s.reconcile(e.(serf.MemberEvent))
 	}
+}
+
+// reconcile is used to reconcile Serf events with the strongly
+// consistent store if we are the current leader
+func (s *SerfEventHandler) reconcile(me serf.MemberEvent) {
+
+	// Do nothing if we are not the leader.
+	if !s.IsLeader() {
+		return
+	}
+
+	// Check if this is a reap event
+	isReap := me.EventType() == serf.EventMemberReap
+
+	// Queue the members for reconciliation
+	for _, m := range me.Members {
+		// Change the status if this is a reap event
+		if isReap {
+			m.Status = StatusReap
+		}
+
+		// Call reconcile
+		if s.Reconciler != nil {
+			s.Reconciler.Reconcile(m)
+		}
+	}
+}
+
+// handleUserEvent is called when a user event is received from both local and remote nodes.
+func (s *SerfEventHandler) handleUserEvent(event serf.UserEvent) {
+	switch name := event.Name; {
+
+	// Handles leader election events
+	case s.IsLeaderEvent(name):
+		s.Logger.Info("serfer: New leader elected: %s", event.Payload)
+
+		// Process leader election event
+		if s.LeaderElectionHandler != nil {
+			s.LeaderElectionHandler.HandleUserEvent(event)
+		}
+
+	// Handle service events
+	case s.isServiceEvent(name):
+		event.Name = s.getRawEventName(name)
+		s.Logger.Debug("serfer: user event: %s", event.Name)
+
+		// Process user event
+		if s.UserEvent != nil {
+			s.UserEvent.HandleUserEvent(event)
+		}
+
+	// Handle unknown user events
+	default:
+		s.Logger.Warn("serfer: unknown event: %v", event)
+
+		// Process unknown event
+		if s.UnknownEventHandler != nil {
+			s.UnknownEventHandler.HandleUserEvent(event)
+		}
+	}
+}
+
+// getRawEventName is used to get the raw event name
+func (s *SerfEventHandler) getRawEventName(name string) string {
+	return strings.TrimPrefix(name, s.ServicePrefix+":")
+}
+
+// isServiceEvent checks if a serf event is a known event
+func (s *SerfEventHandler) isServiceEvent(name string) bool {
+	return strings.HasPrefix(name, s.ServicePrefix+":")
 }
